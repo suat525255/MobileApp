f33b9308d5091cd312e260898ec24a2dd3040452
suat525255 <suataydin5255@gmail.com>
2024-12-23 15:20:28 +0300

    update utils

--- src/test/java/utilities/MobileUtils.java
+++ src/test/java/utilities/MobileUtils.java
@@ -1,10 +1,5 @@
 package utilities;
 
-
-import io.appium.java_client.AppiumDriver;
-import io.appium.java_client.TouchAction;
-import io.appium.java_client.touch.WaitOptions;
-import io.appium.java_client.touch.offset.PointOption;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Dimension;
 import org.openqa.selenium.Rectangle;
